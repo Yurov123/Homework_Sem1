76443ab4b7ac39510d26343b94130cf222136ca1
Konstantin <constantin8271@gmail.com>
2022-02-01 21:08:38 +0300

    Task_2.0 corrected

--- Task_2.0/Program.cs
+++ Task_2.0/Program.cs
@@ -1,4 +1,6 @@
-﻿ Console.WriteLine("Enter num1: ");
+﻿// Даны два числа. Показать большее и меньшее число
+
+ Console.WriteLine("Enter num1: ");
  int num1 = Convert.ToInt32(Console.ReadLine());
  Console.WriteLine("Enter num2: ");
  int num2 = Convert.ToInt32(Console.ReadLine());
