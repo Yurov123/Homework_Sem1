eb731bbbc9cf558b4e5212fcc1f6b3c15287c78d
Konstantin <constantin8271@gmail.com>
2022-02-01 22:20:33 +0300

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+
+int a;                             
+Console.Write("Enter Number: ");
+a = Console.Read();
+
+switch (a % 2)
+{
+    case 1:
+        Console.WriteLine("odd number");
+
+        break;
+    default:
+        Console.WriteLine("Even number");
+        break;
+}
