28c59309e3b69dde922a7f62461acbdfcc099efa
Konstantin <constantin8271@gmail.com>
2022-02-01 22:03:02 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Enter a: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Enter b: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
