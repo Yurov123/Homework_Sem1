ee1a8adcfb1a3ad9aad96e1341b19a6676b9db84
Konstantin <constantin8271@gmail.com>
2022-02-01 20:55:23 +0300

    Task_2.0

---
+++ Task_2.0/Program.cs
@@ -0,0 +1,17 @@
+﻿ Console.WriteLine("Enter num1: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Enter num2: ");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ if(num1 > num2)
+ {
+     Console.WriteLine("The first number is greater than the second");
+ }
+ else if (num1 < num2)
+ {
+     Console.WriteLine("The first number is less than the second");
+ }
+  else
+ {
+     Console.WriteLine("Both numbers are equal");
+ }
